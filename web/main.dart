b001a3762946e3e541e3a46d46f62baa03443c87
Andrey Z <aazaharov81@gmail.com>
2015-03-19 02:21:19 +0300

    nice refactor

--- web/main.dart
+++ web/main.dart
@@ -4,60 +4,32 @@ import 'dart:web_gl';
 import 'dart:typed_data';
 import 'dart:math';
 import 'package:stats/stats.dart';
+import 'package:tweenengine/tweenengine.dart';
 
 part 'ui.dart';
+part 'field.dart';
 
 Element canvas;
+Controls controls;
+TweenManager animManager;
+Field field;
 var gl;
-var fractalTexture;
 
-final int dims = 2;
-
-num planeSize = 1.0; // around 0,0
-
-final Float32List vertices = new Float32List.fromList([
-  -planeSize, -planeSize, 
-  planeSize, planeSize, 
-  -planeSize, planeSize, 
-  
-  -planeSize, -planeSize, 
-  planeSize, planeSize, 
-  planeSize, -planeSize
-]);
-
-Rectangle range = new Rectangle(-2.0, -1.5, 3, 3);
-num x1 = -2.0,
-    y1 = -1.5,
-    x2 = 1.0,
-    y2 = 1.5;
-
-Float32List rangeList = new Float32List.fromList([
-    range.left, range.bottom,
-    range.right, range.top,
-    range.left, range.top,
-    
-    range.left, range.bottom,
-    range.right, range.top,
-    range.right, range.bottom
-            
-]);
-
-
-Buffer vertexBuffer;
-Buffer rangeBuffer;
 
 Stats stats = new Stats();
 
 main() {
   initGL();
-  initGeom(initShaders());
+  
+  field = new Field(gl);
+  
   initUI();
 
   document.body.children.add(stats.container);
   stats.container.style.position = 'absolute';
   stats.container.style.top = '0px';
 
-  draw();
+  window.animationFrame.then(draw);
 }
 
 status(String message) {
@@ -65,82 +37,8 @@ status(String message) {
   status.innerHtml = '<p>${message}</p>';
 }
 
-initBuffers(Program program) {
-  int aPosition = gl.getAttribLocation(program, "a_position");
-  int aRange = gl.getAttribLocation(program, "a_range");
-
-  vertexBuffer = gl.createBuffer();
-  gl.bindBuffer(RenderingContext.ARRAY_BUFFER, vertexBuffer);
-  gl.bufferDataTyped(RenderingContext.ARRAY_BUFFER, vertices, RenderingContext.STATIC_DRAW);
-  gl.enableVertexAttribArray(aPosition);
-  gl.vertexAttribPointer(aPosition, dims, RenderingContext.FLOAT, false, 0, 0);
-
-  rangeBuffer = gl.createBuffer();
-  gl.bindBuffer(RenderingContext.ARRAY_BUFFER, rangeBuffer);
-  gl.bufferDataTyped(RenderingContext.ARRAY_BUFFER, rangeList, RenderingContext.STATIC_DRAW);
-  gl.enableVertexAttribArray(aRange);
-  gl.vertexAttribPointer(aRange, dims, RenderingContext.FLOAT, false, 0, 0);
-}
-
-initGeom(Program program) {
-
-  // Устанавливаем позицию, которая будет передана в вершинный шейдер
-
-  initBuffers(program);
 
 
-
-//Очищаем холст заливая его новым цветом(RedGreenBlueAlpha)
-  gl.clearColor(0.0, 0.0, 0.0, 1);
-  gl.clear(RenderingContext.COLOR_BUFFER_BIT);
-
-
-  UniformLocation uColor = gl.getUniformLocation(program, "u_color");
-  UniformLocation viewport = gl.getUniformLocation(program, "u_viewport");
-  UniformLocation kmax = gl.getUniformLocation(program, "u_kmax");
-  UniformLocation u_range = gl.getUniformLocation(program, "u_range");
-// Как и для очистки холста он задаётся в формате RGBA
-  gl.uniform2f(viewport, canvas.width, canvas.height);
-  gl.uniform4f(u_range, x1, y1, x2, y2);
-  gl.uniform2fv(uColor, new Float32List.fromList([0.5, 0.9, 0.9, 1.0]));
-  gl.uniform1i(kmax, 20);
-}
-
-initShaders() {
-
-  Shader vs = gl.createShader(RenderingContext.VERTEX_SHADER);
-  gl.shaderSource(vs, querySelector("#shader-vx").text);
-  gl.compileShader(vs);
-  if (!gl.getShaderParameter(vs, RenderingContext.COMPILE_STATUS)) {
-    print(gl.getShaderInfoLog(vs));
-    status(gl.getShaderInfoLog(vs));
-    throw new Exception(gl.getShaderInfoLog(vs));
-  }
-
-  Shader fs = gl.createShader(RenderingContext.FRAGMENT_SHADER);
-  //gl.shaderSource(fs, fsSource);
-  gl.shaderSource(fs, querySelector("#shader-fx").text);
-  gl.compileShader(fs);
-  if (!gl.getShaderParameter(fs, RenderingContext.COMPILE_STATUS)) {
-    print(gl.getShaderInfoLog(fs));
-    status(gl.getShaderInfoLog(fs));
-    throw new Exception(gl.getShaderInfoLog(fs));
-  }
-
-// Загружаем шейдеры в GPU
-  Program p = gl.createProgram();
-  gl.attachShader(p, vs);
-  gl.attachShader(p, fs);
-  gl.linkProgram(p);
-  gl.useProgram(p);
-  if (!gl.getProgramParameter(p, RenderingContext.LINK_STATUS)) {
-    print(gl.getProgramInfoLog(p));
-    status(gl.getShaderInfoLog(p));
-    throw new Exception(gl.getShaderInfoLog(p));
-  }
-
-  return p;
-}
 
 initGL() {
 
@@ -153,34 +51,37 @@ initGL() {
   if (gl == null) gl = canvas.getContext("experimental-webgl");
   if (gl == null) {
     status('Простите, ваш браузер не поддерживает WebGl');
-    return;
+    return null;
   }
 
   gl.viewport(0, 0, canvas.width, canvas.height);
   status('');
+  return gl;
 }
 
-initTexture() {
-  fractalTexture = gl.createTexture();
 
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-
-}
 onResize(e) {
   canvas.width = document.body.clientWidth;
   canvas.height = window.innerHeight;
   gl.viewport(0, 0, canvas.width, canvas.height);
-  draw();
+  window.animationFrame.then(draw);
 }
 
+num lastUpdate = 0;
 
-draw() {
+draw(num delta) {
   stats.begin();
-  gl.drawArrays(RenderingContext.TRIANGLES, 0, vertices.length ~/ dims);
-  uidraw();
+  num deltaTime = (delta - lastUpdate) / 1000;
+  lastUpdate = delta;
+  animManager.update(delta);
+  
+  field.draw();
+  
+  if( animManager.length > 0 ) {
+    window.requestAnimationFrame(draw);
+    //animSteps--;
+  }
+  
   stats.end();
 }
 
--- web/ui.dart
+++ web/ui.dart
@@ -1,45 +1,70 @@
 part of mandel;
 
-Point mouseSt;
-final num minSelection = 10.0;
-
 int animSteps = 0;
+
+class Controls {
+  
+  final num minSelection = 1.0;
+  Point mouseSt;
+  Element _el;
+  
+  Controls(this._el) {
+    
+    _el.onMouseDown.listen(onMouseDown);
+    _el.onMouseMove.listen(onMouseMove);
+    _el.onMouseUp.listen(onMouseUp);
+    
+    _el.onMouseWheel.listen((WheelEvent e) {
+      if( e.wheelDeltaX < 0 ) { //zoom in
+        onZoomIn(e);
+      }
+      
+      if( e.wheelDeltaX > 0 ) { //zoom out
+        
+      }
+    });
+  }
+  
+  onMouseDown(MouseEvent e) {
+      mouseSt = new Point(e.client.x, e.client.y);
+      //add to scene selection rect
+  }
+  
+  onMouseMove(MouseEvent e) {
+      if( mouseSt != null && mouseSt.squaredDistanceTo(e.client) >= minSelection ) {
+        _el.dispatchEvent(new CustomEvent('drag', detail: new DragData(mouseSt, e.client)));
+        mouseSt = e.client;        
+      }
+  }
+  
+  onMouseUp(MouseEvent e) {
+    if( mouseSt != null ) {
+      print( "${mouseSt.x}x${mouseSt.y} - ${e.client.x}x${e.client.y}");
+      mouseSt = null;
+
+    }
+  }
+  
+  EventStreamProvider<CustomEvent> onDrag = new EventStreamProvider('drag');
+  
+}
+
+class DragData {
+  Point previous;
+  Point current;
+  DragData(this.previous, this.current);
+}
 
 initUI() {
   window.onResize.listen(onResize);
-  canvas.onMouseDown.listen((e) {
-    mouseSt = new Point(e.client.x, e.client.y);
-    //add to scene selection rect
-  });
+  animManager = new TweenManager();
+  controls = new Controls(canvas);
+  controls.onDrag.forElement(canvas).listen(onDrag);
+  //controls.onDraw
+}
 
-  canvas.onMouseMove.listen((e) {
-
-    if( mouseSt != null && mouseSt.squaredDistanceTo(e.client) >= minSelection ) {
-
-      // enable draw
-
-    }
-  });
-
-  canvas.onMouseUp.listen((e) {
-    if( mouseSt != null ) {
-
-
-      print( "${mouseSt.x}x${mouseSt.y} - ${e.client.x}x${e.client.y}");
-
-
-    }
-  });
-  
-  canvas.onMouseWheel.listen((WheelEvent e) {
-    if( e.wheelDeltaX < 0 ) { //zoom in
-      onZoomIn(e);
-    }
-    
-    if( e.wheelDeltaX > 0 ) { //zoom out
-      
-    }
-  });
+onDrag(CustomEvent e) {
+  //Tween.to()
 }
 
 onZoomIn(MouseEvent e) {
@@ -47,8 +72,5 @@ onZoomIn(MouseEvent e) {
 }
 
 uidraw() {
-  if( animSteps > 0 ) {
-    window.requestAnimationFrame((_)=>draw);
-    animSteps--;
-  }
+
 }
